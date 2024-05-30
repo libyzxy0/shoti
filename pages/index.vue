5dde4accdf4e3dcad1ce6c86fe9b2863e473d8df
Jan Liby Dela Costa <janlibydelacosta@gmail.com>
2024-05-30 19:22:16 +0800

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -84,7 +84,7 @@ export default {
           this.isLoading = true;
           try {
             const response = await axios.post(
-              "https://shoti-srv2-itslibyzxy0.koyeb.app/api" + "/createkey",
+              "https://shoti-srv1.ondernder.com/api" + "/createkey",
               {
                 username: this.apikeyName,
               },
